ad00833006326a4891a58ae6c44a81527983115f
Varun Dey <varun.silversurfer@gmail.com>
2016-07-13 02:35:47 +0530

    migrated to postgres

--- slack/settings.py
+++ slack/settings.py
@@ -77,10 +77,18 @@ WSGI_APPLICATION = 'slack.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'itlo',
+        'USER': 'varun',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+    
 }
 
 
