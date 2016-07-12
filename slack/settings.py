824817a77f133c3014406855181ede639eec37e3
Varun Dey <varun.silversurfer@gmail.com>
2016-07-13 02:37:13 +0530

    updated host

--- slack/settings.py
+++ slack/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'itlo',
         'USER': 'varun',
         'PASSWORD': '123',
-        'HOST': 'localhost',
+        'HOST': '',
         'PORT': '',
     }
     
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+05:30'
 
 USE_I18N = True
 
