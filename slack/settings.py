c67b647053b7bbd3a34f2944a81f02c372e36af4
Varun Dey <varun.silversurfer@gmail.com>
2016-07-13 03:11:54 +0530

    testing settings

--- slack/settings.py
+++ slack/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'slack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES['default'] =  dj_database_url.config()
+# DATABASES['default'] =  dj_database_url.config()
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
